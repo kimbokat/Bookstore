8aa8d6863bb42fc7d882e64c08539c7398035e34
Kim <bgx170@myy.haaga-helia.fi>
2023-02-12 21:39:34 +0200

    bookstore v1.1

--- src/main/java/hh/sof3/BookstoreProject/domain/Book.java
+++ src/main/java/hh/sof3/BookstoreProject/domain/Book.java
@@ -1,9 +1,21 @@
 package hh.sof3.BookstoreProject.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
+	private Long id;
 	private String title;
 	private String author;
+	@Column(name="publishing_year")
 	private int year;
 	private String isbn;
 	private double price;
@@ -26,6 +38,15 @@ public class Book {
 		this.price = price;
 	}
 
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 	public String getTitle() {
 		return title;
 	}
@@ -70,6 +91,8 @@ public class Book {
 	public String toString() {
 		return "Title: " + title + ", Author: " + author + ", Year: " + year + ", ISBN: " + isbn + ", Price: " + price;
 	}
+
+	
 	
 	
 	
--- src/main/java/hh/sof3/BookstoreProject/web/BookController.java
+++ src/main/java/hh/sof3/BookstoreProject/web/BookController.java
@@ -1,23 +1,44 @@
 package hh.sof3.BookstoreProject.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import hh.sof3.BookstoreProject.domain.Book;
+import hh.sof3.BookstoreProject.domain.BookRepository;
+
 
 @Controller
 public class BookController {
 
-	List<Book> books = new ArrayList<>();
+	@Autowired
+	private BookRepository repository;
 
-	@GetMapping("/index")
-	public String welcomePage(Model model, Book book) {
-	books.add(new Book("Lord of the Flies", "William Golding", 1954, "9780399529", 52.34));
-	model.addAttribute("books", books);
-	return "welcome";
+	@RequestMapping(value = { "/", "/booklist" })
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
+
+	}
+
+	@RequestMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
 	}
 }
